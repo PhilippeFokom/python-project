ce7ae7bfa047795c5e7bab3bbdb7081feb115d42
Philippe <pmfokom@gmail.com>
2023-10-31 20:49:40 -0400

    first com

---
+++ error.handling.py
@@ -0,0 +1,12 @@
+
+"""try:
+  print(a)
+except Exception as e:
+  print(e)
+
+print("hello")"""
+try:
+   num =int(input("Please enter a number: "))
+   print(num)
+except Exception as e:
+   print("Invalid entry please try again")
